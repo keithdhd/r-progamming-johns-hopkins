c3164ac3306cf1d159db33b1295b41ebf59bd334
Keith Douglas <keith@keithdouglas.co.uk>
2024-03-30 16:24:46 +0000

    part 2 copmlete

--- complete.R
+++ complete.R
@@ -1,3 +1,24 @@
 complete <- function(directory, id = 1:332) {
+  df <- data.frame()
   
+  for (row_id in id) {
+    row_id <- prefix_numbers_less_than_100(row_id)
+    new_df <- read.csv(paste(getwd(), "/", directory, "/", row_id ,".csv", sep = "" ))
+    
+    num_complete_observations <- nrow(na.omit(new_df))
+    
+    final_df <- data.frame(
+      id = as.numeric(row_id),
+      nobs = num_complete_observations
+    )
+    
+    df <- rbind(df, final_df)
+  } 
+  
+  df
 }
+
+complete("specdata", 1) #   1 1 117
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
--- pollutantmean.R
+++ pollutantmean.R
@@ -26,3 +26,4 @@ column_mean <- function(df, column_name) {
 print(pollutantmean("specdata", "sulfate", 1:10))
 print(pollutantmean("specdata", "nitrate", 70:72))
 print(pollutantmean("specdata", "nitrate", 23))
+
